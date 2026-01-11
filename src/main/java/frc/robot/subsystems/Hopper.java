9356877834742276658434d9872af34951db4bae
pearcell <luke@pearce.org>
2026-01-11 10:17:17 -0500

    Hopper subsystem

---
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+
+public class Hopper extends SubsystemBase {
+
+    private SparkMax sparkMax;
+
+    public Hopper() {
+        sparkMax = new SparkMax(40, MotorType.kBrushless);
+    }
+
+    
+    public Command run() {
+        return Commands.run(
+            () -> sparkMax.set(.5),
+            this
+        );
+    }
+
+    public Command stop() {
+        return Commands.run(
+            () -> sparkMax.set(0),
+            this
+        );
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+
+    }
+}
