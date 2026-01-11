5b59703ae5ef621292c79ba20f1295c1002ef0a6
pearcell <luke@pearce.org>
2026-01-11 10:57:32 -0500

    Added motor config.

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -5,39 +5,40 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 
 public class Hopper extends SubsystemBase {
 
     private SparkMax sparkMax;
+    private SparkMaxConfig sparkMaxConfig;
 
     public Hopper() {
         sparkMax = new SparkMax(40, MotorType.kBrushless);
+        sparkMaxConfig = new SparkMaxConfig();
+        
+        sparkMaxConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(40);
+        sparkMax.configure(sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
-    
-    public Command run() {
-        return Commands.run(
+
+    public Command runHopper() {
+        return Commands.runOnce(
             () -> sparkMax.setVoltage(1),
             this
         );
     }
 
-    public Command stop() {
-        return Commands.run(
+    public Command stopHopper() {
+        return Commands.runOnce(
             () -> sparkMax.setVoltage(0),
             this
         );
-    }
-
-    @Override
-    public void periodic() {
-
-    }
-
-    @Override
-    public void simulationPeriodic() {
-
     }
 }
